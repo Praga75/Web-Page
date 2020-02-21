491b9cc98b00a80272117fdc77fbff0715de91de
Praga75 <42761214+Praga75@users.noreply.github.com>
2020-02-21 01:16:23 -0800

    Commit

--- OnlineFlowerShop/Script/Index.js
+++ OnlineFlowerShop/Script/Index.js
@@ -1,69 +1,114 @@
+//************************OnChange Events *************/   
+//Card Number
 function validateCardNumber() {
-    
-    var x = document.getElementById("cardNumber").value;
-    if(x.length !=16) {
-        document.getElementById("cardNum").innerHTML = "Please Enter 16 digit number";
+    var number = document.getElementById("cardNumber").value;
+    if(number.length !=16) {
+        cardNum.innerHTML = "Please Enter 16 digit number";
     } 
     else {
-        document.getElementById("cardNum").innerHTML = "";   
+        cardNum.innerHTML = "";   
     }
   }
-
-  function validateMobileNumber() {
+//Card Holder Name
+function validateHolderName() {
     
-    var x = document.getElementById("Telephone").value;
-    if(x.length !=10) {
-        document.getElementById("mobileNumber").innerHTML = "Please Enter 10 digit number";
+    var pattern = /^[A-Za-z]+$/;
+    var name = document.getElementById("holderName").value;
+    if(!name.match(pattern)) {
+        HolderComment.innerHTML = "Please Enter Valid Name";
     } 
     else {
-        document.getElementById("mobileNumber").innerHTML = "";   
+        HolderComment.innerHTML = "";   
+    }
+  }
+//Mobile Number
+  function validateMobileNumber() {
+    
+    var mobileNo = document.getElementById("Telephone").value;
+    if(mobileNo.length !=10) {
+        mobileNumber.innerHTML = "Please Enter 10 digit number";
+    } 
+    else {
+        mobileNumber.innerHTML = "";   
     }
    
   }
-
+// Email Id
   function ValidateEmail() {
     var pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     var email = document.getElementById("Email").value;
     if(!email.match(pattern)) {
-        document.getElementById("mailId").innerHTML = "Please Enter valid MailId";
+        mailId.innerHTML = "Please Enter valid MailId";
     }
-    else {
-        document.getElementById("mailId").innerHTML = "";
+    else {                     
+        mailId.innerHTML = "";
     }
     }
-    
+//Security Code  
     function validateSecurityCode() {
-    
+        var pattern = /^[0-9]+$/;
         var securityCode = document.getElementById("passwordId").value;
-        if(securityCode.length != 4 ) {
-            document.getElementById("passwordComment").innerHTML = "Please Enter Valid Security Code";
+        
+        if(securityCode.length != 4 || !securityCode.match(pattern) ) {
+            passwordComment.innerHTML = "Please Enter Valid Security Code";
         } 
         else {
-            document.getElementById("passwordComment").innerHTML = "";   
+            passwordComment.innerHTML = "";   
         }
       }
-    
+    //Post Code
       function validatePostCode() {
     
         var PostCode = document.getElementById("Postcode").value;
         if(PostCode.length != 6 ) {
-            document.getElementById("PostCodeComment").innerHTML = "Please Enter Valid 6 digit PostCode";
+            PostCodeComment.innerHTML = "Please Enter Valid 6 digit PostCode";
         } 
         else {
-            document.getElementById("PostCodeComment").innerHTML = "";   
+            PostCodeComment.innerHTML = "";   
         }
       }
-        
+
+    //Fax
       function validateFax() {
     
-        var PostCode = document.getElementById("FaxId").value;
-        if(PostCode.length != 10 ) {
-            document.getElementById("FaxComment").innerHTML = "Please Enter Valid 10 digit Fax Number";
+        var Fax = document.getElementById("FaxId").value;
+        if(Fax.length != 10 ) {
+            FaxComment.innerHTML = "Please Enter Valid 10 digit Fax Number";
         } 
         else {
-            document.getElementById("FaxComment").innerHTML = "";   
+            FaxComment.innerHTML = "";   
         }
       }
-      function validateNullField() {
-        
-      }
+
+//***********OnClick Event to check Null Fields******************
+   function validateNullField() {
+    var comment = "Field Cannot be Empty";
+   
+    //CardNumber
+    if( cardNumber.value == "") 
+        cardNum.innerHTML = comment;           
+    
+    //Security Code
+    if( passwordId.value == "" ) 
+        passwordComment.innerHTML = comment;        
+         //Card Holder Name
+    if(holderName.value == "") 
+        HolderComment.innerHTML = comment;           
+     
+    //Email Id
+    if(Email.value == "") 
+        mailId.innerHTML = comment;           
+     
+    //Postal Code
+    if(Postcode.value == "") 
+        PostCodeComment.innerHTML = comment;
+    
+    //Address Field
+    if(Addr1.value == "") 
+    AddressComment.innerHTML = comment;
+    
+    //Town /City
+    if(townId.value == "") 
+        townComment.innerHTML = comment;
+   
+   }
